d2c9fa3eaf34c8d0502e0a4379cfb385500804e5
Nordmoen <nordmoen90@gmail.com>
2012-09-25 18:07:43 +0200

    Finally fixed the minimax thingy, it now parses the input array correctly, only need to write the minimax algorithm and the logic. Changed setup.py to include -std=c99 flag

--- lib/minimax.c
+++ lib/minimax.c
@@ -1,12 +1,66 @@
 #include <Python.h>
+#include <string.h>
+
+#define get_piece(x, y, board) board[y*4 + x]
 
 typedef struct{
 	const char *piece;
+	unsigned int length;
 }QuartoPiece;
 
 typedef struct{
-	const char *board;
+	QuartoPiece board[4*4];
 }QuartoBoard;
+
+int pieceEqual(QuartoPiece *a, QuartoPiece *b)
+{
+	int min;
+	if(a->length < b->length){
+		min = a->length;
+	}else{
+		min = b->length;
+	}
+	//We don't need to check the last char
+	//since it will be a closing ) or ]
+	for(int i = 0; i < min - 1; i++){
+		//Check each part of the string
+		//For instance if we have a=[D*] and b = (l*)
+		//the * part will equal and we return true
+		if(a->piece[i] == b->piece[i]){
+			return 1;
+		}
+	}
+	return 0;
+}
+void parse_board(PyObject *board, QuartoBoard *newBoard)
+{
+	//Board must be 4x4!
+	int outer_size, inner_size = 0;
+	outer_size = PyObject_Length(board);
+	if(outer_size > 0){
+		inner_size = PyObject_Length(PySequence_GetItem(board, 0));
+	}
+	if(outer_size != 4 || inner_size != 4){
+		PyErr_SetString(PyExc_TypeError, "The array passed into this function\
+is not a 4x4 array");
+		return;
+	}
+	PyObject *row;
+	PyObject *item;
+	for(int i = 0; i < outer_size; i++){
+		row = PySequence_GetItem(board, i);
+		for(int j = 0; j < inner_size; j++){
+			item = PySequence_GetItem(row, j);
+			if(!PyString_Check(item)){
+				PyErr_SetString(PyExc_TypeError, "Expected String values");
+				return;
+			}
+			QuartoPiece p;
+			p.piece = PyString_AsString(item);
+			newBoard->board[i*4 + j] = p;
+		}
+	}
+}
 
 static PyObject *
 parse_minimax(PyObject *self, PyObject *args)
@@ -16,17 +70,22 @@ parse_minimax(PyObject *self, PyObject *args)
 	//algorithm
 	
 	const char *piece;
-	const char *board;
+	PyObject *board;
+	int piece_size;
 	
-	if(!PyArg_ParseTuple(args, "ss", &piece, &board))
+	if(!PyArg_ParseTuple(args, "s#O", &piece, &piece_size, &board))
 		return NULL;
-
+	
 	QuartoPiece p;
 	p.piece = piece;
+	p.length = piece_size;
 	QuartoBoard b;
-	b.board = board;
+	parse_board(board, &b);
+	if(PyErr_Occurred() != NULL){
+		return NULL;
+	}
 
-	return Py_BuildValue("");//Py_BuildValue("iis", x, y, next_piece.piece);
+	return Py_BuildValue("");//Py_BuildValue("(ii)s", x, y, next_piece.piece);
 }	
 
 static PyMethodDef MiniMaxMethods[] = {
@@ -37,7 +96,7 @@ static PyMethodDef MiniMaxMethods[] = {
 };
  
 PyMODINIT_FUNC
-initminimax(void)
+initquarto(void)
 {
-	(void) Py_InitModule("querto", MiniMaxMethods);
+	(void) Py_InitModule("quarto", MiniMaxMethods);
 }
