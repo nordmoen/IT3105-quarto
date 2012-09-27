fc2a38a67b693482e413d86e5aa3d90d3d5d0006
Trond <krognar@gmail.com>
2012-09-27 15:22:01 +0200

    updated board to work with ints as pieces

--- quarto/board.py
+++ quarto/board.py
@@ -3,10 +3,10 @@
 class Board:
     '''A Quarto board implementation'''
     def __init__(self):
-        self.board = [[None, None, None, None] for i in range(4)]
+        self.board = [[16, 16, 16, 16] for i in range(4)]
 
     def place(self, piece, x, y):
-        if self.board[y][x]:
+        if self.board[y][x] < 16:
             raise PlaceTakenError((x, y), str(self))
         else:
             self.board[y][x] = piece
@@ -16,17 +16,17 @@ class Board:
 
     def check_victory(self, pos):
         start_piece = self.get(*pos)
-        if not start_piece:
+        if start_piece == 16:
             return None
         in_a_row = [False]*4
         in_a_col = [False]*4
         in_a_cross = [False]*4
         in_a_cross2 = [False]*4
         for i in range(4):
-            in_a_col[i] = self.get(pos[0], i) == start_piece if self.get(pos[0], i) else False
-            in_a_row[i] = self.get(i, pos[1]) == start_piece if self.get(i, pos[1]) else False
-            in_a_cross[i] = self.get(i, i) == start_piece if self.get(i, i) else False
-            in_a_cross2[i] = self.get(i, 3-i) == start_piece if self.get(i, 3-i) else False
+            in_a_col[i] = self.get(pos[0], i) == start_piece
+            in_a_row[i] = self.get(i, pos[1]) == start_piece
+            in_a_cross[i] = self.get(i, i) == start_piece
+            in_a_cross2[i] = self.get(i, 3-i) == start_piece
         if all(in_a_row):
             return (1, 0)
         if all(in_a_col):
@@ -34,19 +34,16 @@ class Board:
         if self.__is_cross(pos):
             if all(in_a_cross):
                 return (1, 1)
-        if self.__is_cross(pos, False):
+        if self.__is_cross(pos):
             if all(in_a_cross2):
                 return (3, -1)
         return None
     
-    def __is_cross(self, pos, top_down=True):
-        for i in range(4):
-            if pos[0] == i and pos[1] == (i if top_down else 3-i):
-                return True
-        return False
+    def __is_cross(self, pos):
+        return pos[0] == pos[1] or pos[0] == 3-pos[1]
 
     def __str__(self):
-        return '\n'.join(str(row) for row in self.board)
+        return '\n'.join(', '.join(str(piece) if piece < 16 else ' ' for piece in row) for row in self.board)
     def __repr(self):
         return repr(self.board)
 
