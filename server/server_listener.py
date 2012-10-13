00b45fb9e80acaf9f64cea0ee2f2a68d7b6efcaa
Nordmoen <nordmoen90@gmail.com>
2012-10-13 13:54:46 +0200

    Removed spaces in minimax player. Added new server folder with a couple of files which is to be used by the server

---
+++ server/server_listener.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import threading
+import socket
+import logging
+
+class ServerListener(threading.Thread):
+    '''This class represent a thread which will be listening for
+    incomming request to join the server and will add those requsting
+    to a list of players which the server can use to start games'''
+
+    def __init__(self, player_list, bind_addr, port, log = None):
+        super.__init__(self)
+        self.p_list = player_list
+        self.listen = True
+        self.socket = None
+        self.addr = bind_addr
+        self.port = port
+        if log:
+            self.log = log
+        else:
+            self.log = logging.getLogger('ServerListener')
+
+    def run(self):
+        self.log.debug('Creating socket')
+        self.socket = socket.socket() #Default ipv4 and TCP
+        self.log.debug('Binding to address %s:%i', self.addr, self.port)
+        try:
+            self.socket.bind((self.addr, self.port))
+        except:
+            self.log.exception('Could not bind socket to address: %s:%i', self.addr, self.port)
+            self.log.critical('Shuting down after socket error')
+            return
+        while self.listen:
+            self.log.debug('Waiting for connection')
+            conn, addr = self.socket.accept()
+            self.log.debug('Got connection from address "%s"', addr)
+            self.log.debug('Creating new network player')
+            #TODO:Create new network player here
+            self.log.debug('Adding new player to the list')
+            #TODO: Add player to player list
+        if self.socket:
+            try:
+                self.socket.close()
+            except:
+                self.log.exception('Could not close socket')
+        self.log.debug('Shuting down listener')
+
+
+    def shutdown(self):
+        self.listen = False
+        if self.socket:
+            try:
+                self.socket.shutdown(socket.SHUT_RD)
+            except:
+                self.log.exception('Could not shutdown socket')
---
+++ server/server_thread.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import threading
+from ..quarto import game
+
+class ServerThread(threading.Thread):
+    '''A class which will simulate a game between two players
+    in a threaded manner to ensure that we can have several
+    playing at the same time'''
+
+    def __init__(self, player1, player2):
+        super.__init__(self)
+        self.player1 = player1
+        self.player2 = player2
+
+    def run(self):
+       game = game.Game(self.player1, self.player2)
+       self.stats = game.play()
+
+    def get_stats(self):
+        return self.stats
