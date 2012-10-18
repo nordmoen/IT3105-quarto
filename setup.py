3829b618a677f7636084a62c8491cb454c95affa
Nordmoen <nordmoen90@gmail.com>
2012-10-18 14:14:01 +0200

    Added more tuning options to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 
 from distutils.core import setup, Extension
- 
-module1 = Extension('cquarto', sources = ['lib/minimax.c'], extra_compile_args=['-std=c99'])
-  
+
+module1 = Extension('cquarto', sources = ['lib/minimax.c'],
+        extra_compile_args=['-std=c99', '-march=native'])
+
 setup(name = 'CQuarto', version = '0.1', description = 'This is a minimax package which can be\
         used in Python', ext_modules = [module1])
